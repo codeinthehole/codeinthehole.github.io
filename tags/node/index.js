741fefcebba6fc81ec7404097371cce7d3f4f6b8
David Winterbottom <david.winterbottom@gmail.com>
2017-03-16 23:10:22 +0000

    Manually add tags folder

---
+++ tags/node/index.js
@@ -0,0 +1,74 @@
+<!doctype html>
+<!--[if lt IE 7]><html class="no-js ie6 oldie" lang="en"><![endif]-->
+<!--[if IE 7]><html class="no-js ie7 oldie" lang="en"><![endif]-->
+<!--[if IE 8]><html class="no-js ie8 oldie" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+
+        
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+
+        
+        <link rel="stylesheet" href="/css/styles.css" type="text/css" />
+        <link rel="stylesheet" href="/css/pygments.css" type="text/css" />
+
+        
+        <link rel="alternate" href="/index.xml" type="application/rss+xml" title="David Winterbottom">
+
+        
+        <link href='http://fonts.googleapis.com/css?family=Signika+Negative:600' rel='stylesheet' type='text/css' />
+
+        
+        <title>Node.Js</title>
+        <meta name="author" content="David Winterbottom">
+        <meta name="description" content="">
+
+    </head>
+
+    <body>
+
+        <article class="hentry" id="container">
+
+<header>
+    <div id="logo">
+        <h4><a href="/" title="David Winterbottom">David Winterbottom</a></h4>
+        <h5><a href="/writing/">writing</a> | 
+            <a href="http://twitter.com/codeinthehole" title="Occasionally I say something interesting">tweets</a> | 
+            <a href="/code/">code</a> |
+            <a href="/about/">about</a></h5>
+    </div>
+</header>
+
+
+<div id="main" role="main">
+    <div id="content" class="articles">
+        <h1 id="title">Node.Js</h1>
+        
+            <div class="article_summary">
+    <div class="meta">
+        May 22, 2011<br/>
+        <div class="tags">
+            
+                
+                 <a href="/tags/node.js">node.js</a>
+            
+        </div>
+    </div>
+    <div class="main">
+        <a href="/projects/multi-scp-copying-a-file-to-all-hosts-in-your-ssh-config/">Multi-scp: copying a file to all hosts in your SSH config</a>
+        <p>Using node.js to copy a file to multiple remote hosts</p>
+    </div>
+</div>
+
+        
+    </div>
+</div>
+
+        </article>
+    </body>
+</html>
+
